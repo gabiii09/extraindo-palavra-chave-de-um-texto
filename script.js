a9c213d92635f538266afedc539a5187bf928b40
gabiii09 <gabriela.matoscheski@escola.pr.gov.br>
2025-09-19 08:48:35 -0300

    ok

--- script.js
+++ script.js
@@ -2,16 +2,30 @@ const botaoMostraPalavras = document.querySelector('#botao-
 palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
 function mostraPalavrasChave() {
 const texto = document.querySelector('#entrada-de-texto').value;
-
 const campoResultado = document.querySelector('#resultado-
 palavrachave');
-
 const palavrasChave = processaTexto(texto);
+
 campoResultado.textContent = palavrasChave.join(", ");
 }
+
 function processaTexto(texto) {
 let palavras = texto.split(/\P{L}+/u);
+
+let frequencia = {};
+fort (let i of palavras);
+frequencia[i] = 0;
+for (let j of palavras) {
+    if (i ==j) {  
+        frequencias[i]++;
+         }
+      }
+    } 
+
+ console.log(frequencias);
+
 return palavras;
 }
